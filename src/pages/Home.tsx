3307a46b01e0095956a047b820f4b56494f6a58e
giahuy0811 <giahuy2001le@gmail.com>
2022-12-23 10:24:46 +0700

    set up project

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,14 @@
+import { Button } from 'antd'
+
+
 
 
 const HomePage = () => {
-  return <div>home page</div>
+  return <div>
+    <Button type='primary'>click</Button>
+    <p>wqdqw</p>
+    <h1>h1</h1>
+  </div>
 }
 
 export default HomePage
